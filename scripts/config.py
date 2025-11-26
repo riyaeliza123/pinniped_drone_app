54f156e9e5a7d4e76a465409326ea82df4cf0800
Riya Eliza <riyaelizashaju@gmail.com>
2025-11-26 13:09:28 -0800

    testing error

--- scripts/config.py
+++ scripts/config.py
@@ -19,7 +19,9 @@ CAMERA_SENSOR_WIDTHS = {
     "FC220": 6.3,
 }
 
-API_KEY = st.secrets["ROBOWFLOW_API_KEY"]
-rf = Roboflow(api_key=API_KEY)
-project = rf.workspace().project(PROJECT_NAME)
-model = project.version(MODEL_VERSION).model
+try:
+    rf = Roboflow(api_key=API_KEY)
+    project = rf.workspace().project(PROJECT_NAME)
+    model = project.version(MODEL_VERSION).model
+except Exception as e:
+    st.error(f"Roboflow error: {e}")
